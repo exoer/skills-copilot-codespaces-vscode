448347b466fd87273ce3fabeb15079e37d9d53c7
Arne <arne.jenssen@gmail.com>
2024-05-22 17:36:28 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,93 +1,54 @@
 // create a webserver
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-// create a variable to store the comments
-const comments = [];
-
-// add the body parser middleware
-app.use(bodyParser.json());
-
-// create a POST route to add a new comment
-app.post('/comments', (req, res) => {
-  const comment = req.body.comment;
-  comments.push(comment);
-  res.json(comment);
-});
-
-// create a GET route to get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-// end of comments.js
-
-// Path: server.js
-// create a webserver
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-// create a variable to store the comments
-const comments = [];
-
-// add the body parser middleware
-app.use(bodyParser.json());
-
-// create a POST route to add a new comment
-app.post('/comments', (req, res) => {
-  const comment = req.body.comment;
-  comments.push(comment);
-  res.json(comment);
-});
-
-// create a GET route to get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-// end of server.js
-
-// Path: index.js
-// create a webserver
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-// create a variable to store the comments
-const comments = [];
-
-// add the body parser middleware
-app.use(bodyParser.json());
-
-// create a POST route to add a new comment
-app.post('/comments', (req, res) => {
-  const comment = req.body.comment;
-  comments.push(comment);
-  res.json(comment);
-});
-
-// create a GET route to get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-// end of index.js
-
-// Path: package.json
-{
-  "name": "comments",
-  "version": "1.0.0",
+// create server
+http.createServer(function(req, res) {
+  // parse the request url
+  var path = url.parse(req.url).pathname;
+  // handle the request
+  switch (path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<h1>Welcome to the homepage</h1>');
+      res.end();
+      break;
+    case '/comments':
+      // read the comments file
+      fs.readFile(__dirname + '/comments.txt', function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.write('Oh no! Could not find file');
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+    case '/submit':
+      // parse the url query string
+      var query = url.parse(req.url, true).query;
+      // append the comment to the file
+      fs.appendFile(__dirname + '/comments.txt', query.comment + '\n', function(err) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.write('Oh no! Could not append to file');
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.write('Comment added successfully');
+          res.end();
+        }
+      });
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('Oh no! Could not find file');
+      res.end();
+      break;
+  }
+}).listen(1337);
+console.log('Server running at http://localhost:1337/');
