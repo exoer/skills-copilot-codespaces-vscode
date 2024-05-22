d3bb8ca194e5ab982b44ffbe0a23410aa5a7d84a
Arne <arne.jenssen@gmail.com>
2024-05-22 17:13:52 +0000

    copilod second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = {
+        "HTML": "70%",
+        "CSS": "60%",
+        "JavaScript": "50%",
+        "PHP": "40%",
+        "Python": "30%"
+    };
+    return skills;
+}
